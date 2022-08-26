404d214d522b953ad19dfb51cc849d9b45ed0ce5
CynaraCosta <cvocac@cin.ufpe.br>
2022-08-26 13:32:48 -0300

    add the login_git_hub

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ta-gui';
+  aluno: Aluno = {nome: "", cpf: "", email: "", login_git_hub: ""};
 }
+
+export class Aluno {
+  nome: string = "";
+  cpf: string = "";
+  email: string = "";
+  login_git_hub: string = "";
+}
+
